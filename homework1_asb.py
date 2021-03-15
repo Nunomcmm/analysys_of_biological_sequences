533cc81680f3f516aaec28e15cfe0f98ffd6017f
Nunomcmmm <nunoporsche@hotmail.com>
2021-03-15 18:06:01 +0000

    Update homework1

--- homework1_asb.py
+++ homework1_asb.py
@@ -10,8 +10,12 @@ handle = Entrez.esearch(db=sys.argv[1], term=sys.argv[2], retmax=sys.argv[3] , u
 result = Entrez.read(handle)
 #Atribui-se à variável identifiers a IdList do result
 identifiers = result['IdList']
+#Atribui-se à variável webenv a lista de WebEnv do result
+webenv = result['WebEnv']
+#Atribui-se à variável query_key a lista das QueryKey do result
+query_key = result['QueryKey']
 #Atribui-se à variável handle a função efetch que vai buscar toda a informação de cada identificador, o rettype reescreve em formato fasta e o retmode em ficheiro txt.
-handle = Entrez.efetch(db=sys.argv[1], id= identifiers,retmax=sys.argv[3],rettype="fasta", retmode="text")
+handle = Entrez.efetch(db=sys.argv[1], id= identifiers,retmax=sys.argv[3],rettype="fasta", retmode="text", webenv = webenv, query_key = query_key)
 #Atribui-se à variável record o ficheiro em formato FASTA
 record = handle.read()
 #Imprimir a variável record na consola
